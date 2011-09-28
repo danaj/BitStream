53c9da101ae6772f0c08cf03f474fd52846d0298
Dana Jacobsen <dana@acm.org>
2011-09-27 19:25:04 -0600

    Rice code takes a sub parameter just like Golomb

--- lib/Data/BitStream/Code/Rice.pm
+++ lib/Data/BitStream/Code/Rice.pm
@@ -1,31 +1,49 @@
 package Data::BitStream::Code::Rice;
+use strict;
+use warnings;
 BEGIN {
   $Data::BitStream::Code::Rice::AUTHORITY = 'cpan:DANAJ';
 }
 BEGIN {
-  $Data::BitStream::Code::Rice::VERSION = '0.01';
+  $Data::BitStream::Code::Rice::VERSION = '0.02';
 }
 
 use Mouse::Role;
-
-requires 'read', 'write', 'put_unary', 'get_unary';
+requires qw(read write put_unary get_unary);
 
 sub put_rice {
   my $self = shift;
+  my $sub;
   my $k = shift;
-  die "k must be >= 1" unless $k >= 1;
+  if (ref $k eq 'CODE') {   # Check for sub as first parameter
+    $sub = $k;
+    $k = shift;
+  }
+
+  die "k must be >= 0" unless $k >= 0;
+  return( (defined $sub) ? $sub->($self, @_) : $self->put_unary(@_) ) if $k==0;
 
   foreach my $val (@_) {
     die "Value must be >= 0" unless $val >= 0;
-    $self->put_unary($val >> $k);
-    $self->write($k, $val);
+    my $q = $val >> $k;
+    my $r = $val - ($q << $k);
+    (defined $sub)  ?  $sub->($self, $q)  :  $self->put_unary($q);
+    $self->write($k, $r);
   }
   1;
 }
 sub get_rice {
   my $self = shift;
+  my $sub;
   my $k = shift;
-  die "k must be >= 1" unless $k >= 1;
+  if (ref $k eq 'CODE') {   # Check for sub as first parameter
+    $sub = $k;
+    $k = shift;
+  }
+
+  die "k must be >= 0" unless $k >= 0;
+  return( (defined $sub) ? $sub->($self, @_) : $self->get_unary(@_) ) if $k==0;
+
   my $count = shift;
   if    (!defined $count) { $count = 1;  }
   elsif ($count  < 0)     { $count = ~0; }   # Get everything
@@ -33,7 +51,7 @@ sub get_rice {
 
   my @vals;
   while ($count-- > 0) {
-    my $q = $self->get_unary();
+    my $q = (defined $sub)  ?  $sub->($self)  :  $self->get_unary();
     last unless defined $q;
     push @vals, ($q << $k)  |  $self->read($k);
   }
