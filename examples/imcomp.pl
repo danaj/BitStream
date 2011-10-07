9ac7de69990adaa7a080b08a519cc9da31cfd698
Dana Jacobsen <dana@acm.org>
2011-10-07 17:03:51 -0600

    Dispatch predictors

--- examples/imcomp.pl
+++ examples/imcomp.pl
@@ -10,7 +10,10 @@ use POSIX;
 use Imager;
 
 #
-# Very simple example lossless image compressor.
+# Very simple example lossless image compressor.  I've added a variety of
+# predictors and a couple decorrelation transforms for better compression,
+# and to make the program more useful for experimentation with different
+# ideas.
 #
 # Examples:
 # 
@@ -45,6 +48,15 @@ my %transform_info = (
   'YCOCG' => [ 'YCoCg', \&decorrelate_YCoCg, \&correlate_YCoCg ],
   'RCT'   => [ 'RCT',   \&decorrelate_RCT,   \&correlate_RCT   ],
   'RGB'   => [ 'RGB',   undef,               undef             ],
+);
+
+my %predictor_info = (
+  '1D'    => \&predict_1d,
+  'DARC'  => \&predict_darc,
+  'MED'   => \&predict_med,
+  'GAP'   => \&predict_gap,
+  'DJMED' => \&predict_djmed,
+  'GED2'  => \&predict_ged2,
 );
 
 sub die_usage {
@@ -98,102 +110,133 @@ die_usage if defined $opts{'c'} && defined $opts{'d'};
 if (defined $opts{'c'}) {
   compress_file( $opts{'i'},
                  $opts{'o'},
-                 $opts{'code'}        || 'Gamma',
-                 uc ($opts{'predict'} || 'MED'),
-                 $opts{'transform'}   || 'YCoCg',
+                 $opts{'code'}          || 'ARice(2)',
+                 uc ($opts{'predict'}   || 'MED'),
+                 uc ($opts{'transform'} || 'YCoCg'),
                );
 } else {
   decompress_file( $opts{'i'}, $opts{'o'} );
 }
 
 
+###############################################################################
+#
+#              Pixel Predictors
+#
+###############################################################################
+sub predict_1d {
+  my ($x, $width, $y, $p, $colors) = @_;
 
-# TODO: Consider dispatch table
-sub predict {
-  my ($predictor, $x, $y, $width, $r, $rn, $rnn) = @_;
-  die unless defined $predictor;
-  my $pred;
-
-  my ($w, $n);
-  $w  = $r ->[$x-1] if $x > 0;
-  $n  = $rn->[$x  ] if $y > 0;
-  # Simple 1-D edge cases
-  return 0  if $x == 0 && $y == 0;
-  return $w if $y == 0;
-  return $n if $x == 0;
-
-  my $nw = $rn->[$x-1];
-
-  if ($predictor eq 'DARC') {
-    my $gv = abs($w - $nw);
-    my $gh = abs($n - $nw);
-    return $n if $gv + $gh == 0;
-    my $alpha = $gv / ($gv + $gh);
-    $pred = $alpha * $w + (1-$alpha) * $n;
-    return POSIX::floor($pred);
-  }
-
-  # Use MED if our window isn't large enough (no nn, ww, ne)
-  if ( ($predictor eq 'MED') || ($y == 1) || ($x == 1) || ($x == $width-1)) {
-    my $maxwn = ($n > $w) ? $n : $w;
-    my $minwn = ($n < $w) ? $n : $w;
-    if    ($nw >= $maxwn) { $pred = $minwn; }
-    elsif ($nw <= $minwn) { $pred = $maxwn; }
-    else                  { $pred = $n + $w - $nw; }
-    return $pred;
-  }
-
-  my $ww = $r  ->[$x-2];
-  my $ne = $rn ->[$x+1];
-  my $nn = $rnn->[$x  ];
-  my $nne= $rnn->[$x+1];
-
-  if ($predictor eq 'DJMED') {
-    my $T = 16;
-    my $gv = abs($nw - $w) + abs($nn - $n);
-    my $gh = abs($ww - $w) + abs($nw - $n);
-    if    (($gv-$gh) >  $T) { $pred = $w; }
-    elsif (($gv-$gh) < -$T) { $pred = $n; }
-    else {
-      # predict the median of three linear predictors
-      my $p1 = $n + $w - $nw;
-      my $p2 = $n - ($nn - $n);
-      my $p3 = $w - ($ww - $w);
-      $pred = ( $p1<$p2 ? ($p2<$p3 ? $p2
-                                   : ($p1<$p3 ? $p3 : $p1))
-                        : ($p3<$p2 ? $p2
-                                   : ($p3<$p1 ? $p3 : $p1)) );
-    }
-    return $pred;
-  }
-
-  if ($predictor eq 'GAP') {
-    # GAP (Gradient Adjusted Predictor) from CALIC
-    my $dh = abs($w - $ww) + abs($n - $nw) + abs($ne - $n);
-    my $dv = abs($w - $nw) + abs($n - $nn) + abs($ne - $nne);
-    return $n if $dh - $dv > 80;
-    return $w if $dv - $dh > 80;
-    $pred = ($w + $n)/2 + ($ne - $nw)/4;
-    if    ($dh-$dv > 32) { $pred = (  $pred + $n) / 2; }
-    elsif ($dv-$dh > 32) { $pred = (  $pred + $w) / 2; }
-    elsif ($dh-$dv >  8) { $pred = (3*$pred + $n) / 4; }
-    elsif ($dv-$dh >  8) { $pred = (3*$pred + $w) / 4; }
-    return POSIX::floor($pred);
-  }
-
-  if ($predictor eq 'GED2') {
-    my $T = 8;
-    my $gv = abs($nw - $w) + abs($nn - $n);
-    my $gh = abs($ww - $w) + abs($nw - $n);
-    if    ($gv - $gh >  $T) { $pred = $w; }
-    elsif ($gv - $gh < -$T) { $pred = $n; }
-    else                    { $pred = $n + $w - $nw; }
-    return $pred;
-  }
-
-  die "Unknown predictor: $predictor";
+  return 0 if $x == 0 && $y == 0;
+  return $colors->[$y-1]->[$x  ]->[$p] if $x == 0;
+  return $colors->[$y  ]->[$x-1]->[$p];
 }
 
+sub predict_darc {
+  my ($x, $width, $y, $p, $colors) = @_;
+  return predict_1d(@_) if $x == 0 || $y == 0;
+
+  my $w  = $colors->[$y  ]->[$x-1]->[$p];
+  my $n  = $colors->[$y-1]->[$x  ]->[$p];
+  my $nw = $colors->[$y-1]->[$x-1]->[$p];
+
+  my $gv = abs($w - $nw);
+  my $gh = abs($n - $nw);
+  return $n if $gv + $gh == 0;
+  my $alpha = $gv / ($gv + $gh);
+  return POSIX::floor( $alpha * $w + (1-$alpha) * $n );
+}
+
+# MED (Median Edge Detection) from LOCO-I / JPEG-LS
+sub predict_med {
+  my ($x, $width, $y, $p, $colors) = @_;
+  return predict_1d(@_) if $x == 0 || $y == 0;
+
+  my $w  = $colors->[$y  ]->[$x-1]->[$p];
+  my $n  = $colors->[$y-1]->[$x  ]->[$p];
+  my $nw = $colors->[$y-1]->[$x-1]->[$p];
+
+  my ($minwn, $maxwn) = ($n > $w)  ?  ($w, $n)  :  ($n, $w);
+  return $minwn if $nw >= $maxwn;
+  return $maxwn if $nw <= $minwn;
+  return $n + $w - $nw;
+}
+
+# GAP (Gradient Adjusted Predictor) from CALIC
+sub predict_gap {
+  my ($x, $width, $y, $p, $colors) = @_;
+  return predict_med(@_) if $y <= 1 || $x <= 1 || $x == $width-1;
+
+  my $w  = $colors->[$y  ]->[$x-1]->[$p];
+  my $n  = $colors->[$y-1]->[$x  ]->[$p];
+  my $nw = $colors->[$y-1]->[$x-1]->[$p];
+  my $ww = $colors->[$y  ]->[$x-2]->[$p];
+  my $ne = $colors->[$y-1]->[$x+1]->[$p];
+  my $nn = $colors->[$y-2]->[$x  ]->[$p];
+  my $nne= $colors->[$y-2]->[$x+1]->[$p];
+
+  my $dh = abs($w - $ww) + abs($n - $nw) + abs($ne - $n);
+  my $dv = abs($w - $nw) + abs($n - $nn) + abs($ne - $nne);
+  return $n if $dh - $dv > 80;
+  return $w if $dv - $dh > 80;
+  my $pred = ($w + $n)/2 + ($ne - $nw)/4;
+  if    ($dh-$dv > 32) { $pred = (  $pred + $n) / 2; }
+  elsif ($dv-$dh > 32) { $pred = (  $pred + $w) / 2; }
+  elsif ($dh-$dv >  8) { $pred = (3*$pred + $n) / 4; }
+  elsif ($dv-$dh >  8) { $pred = (3*$pred + $w) / 4; }
+  return POSIX::floor($pred);
+}
+
+# DJMED (Median of three linear predictors)
+sub predict_djmed {
+  my ($x, $width, $y, $p, $colors) = @_;
+  return predict_med(@_) if $y <= 1 || $x <= 1;
+
+  my $w  = $colors->[$y  ]->[$x-1]->[$p];
+  my $n  = $colors->[$y-1]->[$x  ]->[$p];
+  my $nw = $colors->[$y-1]->[$x-1]->[$p];
+  my $ww = $colors->[$y  ]->[$x-2]->[$p];
+  my $nn = $colors->[$y-2]->[$x  ]->[$p];
+
+  my $T = 16;
+  my $gv = abs($nw - $w) + abs($nn - $n);
+  my $gh = abs($ww - $w) + abs($nw - $n);
+  return $w if ($gv-$gh) >  $T;
+  return $n if ($gv-$gh) < -$T;
+
+  # predict the median of three linear predictors
+  my $p1 = $n + $w - $nw;
+  my $p2 = $n - ($nn - $n);
+  my $p3 = $w - ($ww - $w);
+  my $pred = ( $p1<$p2 ? ($p2<$p3 ? $p2 : ($p1<$p3 ? $p3 : $p1))
+                       : ($p3<$p2 ? $p2 : ($p3<$p1 ? $p3 : $p1)) );
+  return $pred;
+}
+
+sub predict_ged2 {
+  my ($x, $width, $y, $p, $colors) = @_;
+  return predict_med(@_) if $y <= 1 || $x <= 1;
+
+  my $w  = $colors->[$y  ]->[$x-1]->[$p];
+  my $n  = $colors->[$y-1]->[$x  ]->[$p];
+  my $nw = $colors->[$y-1]->[$x-1]->[$p];
+  my $ww = $colors->[$y  ]->[$x-2]->[$p];
+  my $nn = $colors->[$y-2]->[$x  ]->[$p];
+
+  my $T = 8;
+  my $gv = abs($nw - $w) + abs($nn - $n);
+  my $gh = abs($ww - $w) + abs($nw - $n);
+  return $w if ($gv-$gh) >  $T;
+  return $n if ($gv-$gh) < -$T;
+  return ($n + $w - $nw);
+}
+
+
+###############################################################################
+#
+#              Color Transforms for decorrelation
+#
+###############################################################################
 
 # It would be great to just use Imager's matrix convert for the color
 # transforms, but it clamps the results to 0-255, which makes it useless.
@@ -230,9 +273,9 @@ sub decorrelate_YCoCg {
 
   @{$rcolors} = map { my ($r, $g, $b) = @{$_};
                       my $Co = $r - $b;
-                      my $t  = $b + POSIX::floor($Co/2);
+                      my $t  = $b + int( (($Co < 0) ? $Co-1 : $Co) / 2 );
                       my $Cg = $g - $t;
-                      my $Y  = $t + POSIX::floor($Cg/2);
+                      my $Y  = $t + int( (($Cg < 0) ? $Cg-1 : $Cg) / 2 );
                       [ ($Y,$Co,$Cg) ];
                     } @{$rcolors};
 }
@@ -241,13 +284,19 @@ sub correlate_YCoCg {
   die unless scalar @{$rcolors->[0]} == 3;
 
   @{$rcolors} = map { my ($Y, $Co, $Cg) = @{$_};
-                      my $t = $Y - POSIX::floor($Cg/2);
+                      my $t = $Y - int( (($Cg < 0) ? $Cg-1 : $Cg) / 2 );
                       my $g = $Cg + $t;
-                      my $b = $t - POSIX::floor($Co/2);
+                      my $b = $t - int( (($Co < 0) ? $Co-1 : $Co) / 2 );
                       my $r = $b + $Co;
                       [ ($r,$g,$b) ];
                     } @{$rcolors};
 }
+
+###############################################################################
+#
+#              Image Compression
+#
+###############################################################################
 
 sub compress_file {
   my($infile, $outfile, $code, $predictor, $transform) = @_;
@@ -259,15 +308,17 @@ sub compress_file {
   # Image header:
   my ($width, $height, $planes, $mask) = $image->i_img_info;
 
-  my $decor_sub;
-  if (defined $transform) {
-    die "Unknown transform: $transform" unless defined $transform_info{uc $transform};
-    $transform = $transform_info{uc $transform}->[0];  # Canonical name
-    $decor_sub = $transform_info{uc $transform}->[1];  # decorrelation sub
-  }
+  $transform = 'RGB' unless $planes > 1;
+  my $trans_data = $transform_info{$transform};
+  die "Unknown transform: $transform" unless defined $trans_data;
+  my $trans_name = $trans_data->[0];  # Canonical name
+  my $decor_sub  = $trans_data->[1];  # decorrelation sub
+
+  my $predict_sub = $predictor_info{$predictor};
+  die "Unknown predictor: $predictor" unless defined $predict_sub;
 
   my $method = "$code/$predictor";
-  $method .= "/$transform" if $planes > 1;
+  $method .= "/$trans_name" if $planes > 1;
 
   # Start up the stream
   my $stream = Data::BitStream->new(
@@ -278,35 +329,6 @@ sub compress_file {
   my @colors;   # [$y]->[$x]->[$p]
   foreach my $y (0 .. $height-1) {
     $colors[$y-3] = undef if $y >= 3;   # remove unneeded y values
-
-    # Simple code for single-plane.  Doing this to better illustrate the basic
-    # predict-encode operation without getting bogged down in Imager and color
-    # plane decorrolation.
-    if ($planes == 1) {
-      # Get scanline from imager, and extract the 8-bit color value
-      $colors[$y] = [ map { ($_->rgba)[0] }
-                          $image->getscanline(y=>$y, type=>'8bit') ];
-
-      my @val   =              @{$colors[$y  ]};
-      my @nval  = ($y > 0)  ?  @{$colors[$y-1]}  :  ();
-      my @nnval = ($y > 1)  ?  @{$colors[$y-2]}  :  ();
-      die "short code read" unless scalar @val == $width;
-
-      my @deltas = ();
-      foreach my $x (0 .. $width-1) {
-        my $pixel  = $val[$x];
-
-        # 1) Predict this pixel.
-        my $predict = predict($predictor, $x, $y, $width, \@val, \@nval, \@nnval);
-
-        # 2) encode the delta mapped to an unsigned number
-        my $delta  = $pixel - $predict;
-        my $udelta = ($delta >= 0)  ?  2*$delta  :  -2*$delta-1;
-        push @deltas, $udelta;
-      }
-      $stream->code_put($code, @deltas);
-      next;
-    }
 
     {
       # Get a scanline of colors and convert to RGB
@@ -321,26 +343,21 @@ sub compress_file {
     # Decorrelate the color planes for better compression
     $decor_sub->($colors[$y]) if defined $decor_sub;
 
-    #foreach my $x (0 .. $width-1) { print "[$y,$x] ", join(' ',@{$ycolors[$x]}), "\n"; }
-
     foreach my $p (0 .. $planes-1) {
-      my @val   =              map { $_->[$p] } @{$colors[$y  ]};
-      my @nval  = ($y > 0)  ?  map { $_->[$p] } @{$colors[$y-1]}  :  ();
-      my @nnval = ($y > 1)  ?  map { $_->[$p] } @{$colors[$y-2]}  :  ();
+      my @pixels = map { $_->[$p] } @{$colors[$y]};
 
-      my @deltas = ();
+      my @udeltas = ();
       foreach my $x (0 .. $width-1) {
-        my $pixel  = $val[$x];
-
         # 1) Predict this pixel.
-        my $predict = predict($predictor, $x, $y, $width, \@val, \@nval, \@nnval);
+        my $predict = $predict_sub->($x, $width, $y, $p, \@colors);
 
         # 2) encode the delta mapped to an unsigned number
+        my $pixel = $pixels[$x];
         my $delta  = $pixel - $predict;
         my $udelta = ($delta >= 0)  ?  2*$delta  :  -2*$delta-1;
-        push @deltas, $udelta;
+        push @udeltas, $udelta;
       }
-      $stream->code_put($code, @deltas);
+      $stream->code_put($code, @udeltas);
     }
   }
 
@@ -352,6 +369,12 @@ sub compress_file {
          $origsize, $method, $compsize, $origsize / $compsize;
 }
 
+
+###############################################################################
+#
+#              Image Decompression
+#
+###############################################################################
 
 sub decompress_file {
   my($infile, $outfile) = @_;
@@ -381,6 +404,9 @@ sub decompress_file {
     $cor_sub = $transform_info{uc $transform}->[2];
   }
 
+  my $predict_sub = $predictor_info{$predictor};
+  die "Unknown predictor: $predictor" unless defined $predict_sub;
+
   # Start up an Imager object
   my $image = Imager->new( xsize    => $width,
                            ysize    => $height,
@@ -390,31 +416,24 @@ sub decompress_file {
   foreach my $y (0 .. $height-1) {
     $colors[$y-3] = undef if $y >= 3;   # remove unneeded y values
 
-    my @ycolors;  # ycolors[$x]->[$p];
     foreach my $p (0 .. $planes-1) {
       # get a line worth of absolute deltas and convert them to signed
       my @deltas = map { (($_&1) == 0)  ?  $_ >> 1  :  -(($_+1) >> 1); }
                    $stream->code_get($code, $width);
       die "short code read" unless scalar @deltas == $width;
 
-      my @val   = ();
-      my @nval  = ($y > 0)  ?  map { $_->[$p] } @{$colors[$y-1]}  :  ();
-      my @nnval = ($y > 1)  ?  map { $_->[$p] } @{$colors[$y-2]}  :  ();
-
       foreach my $x (0 .. $width-1) {
-        my $predict = predict($predictor, $x, $y, $width, \@val, \@nval, \@nnval);
+        my $predict = $predict_sub->($x, $width, $y, $p, \@colors);
         my $pixel = $predict + $deltas[$x];
-        push @val, $pixel;
-        push @{$ycolors[$x]}, $pixel;
+        $colors[$y][$x][$p] = $pixel;
       }
     }
-    $colors[$y] = [@ycolors];
 
     # set the scanline
     {
       my @icolors;
       if ($planes == 1) {
-        @icolors = map { Imager::Color->new(gray => $_->[0]); } @ycolors;
+        @icolors = map { Imager::Color->new(gray => $_->[0]); } @{$colors[$y]};
       } else {
         # operate on a copy of colors so we ensure it's not changed.
         my $ycolors_copy = dclone($colors[$y]);
